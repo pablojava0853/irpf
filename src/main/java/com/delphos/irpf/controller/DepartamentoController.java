bddba77edec619d440ab2d7a89f2c4c529f7d0b0
Pablo Gonzalez <pablojava853@outlook.com>
2021-06-18 03:52:27 -0300

    criação dos metodos salvar, deletar, alterar e consultar departamento

--- src/main/java/com/delphos/irpf/controller/DepartamentoController.java
+++ src/main/java/com/delphos/irpf/controller/DepartamentoController.java
@@ -1,6 +1,12 @@
 package com.delphos.irpf.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +20,24 @@ public class DepartamentoController {
 	@Autowired
 	DepartamentoRepository departamentoRepository;
 	
+	@GetMapping("/departamento")
 	public Departamento salvarDepartamento(@RequestBody Departamento departamento) {
+		return departamentoRepository.save(departamento);
+	}
+	@GetMapping("/departamentos")
+	public List<Departamento>listaDepartamentos(){
+		return departamentoRepository.findAll();
+	}
+	@GetMapping("/departamento/{idDepatamento}")
+	public Departamento listaUnicoDepartamento(@PathVariable(value="idDepatamento") long idDepatamento) {
+		return departamentoRepository.findById(idDepatamento);
+	}
+	@DeleteMapping("/depatamento")
+	public void deletaDepartamento(@RequestBody Departamento depatamento) {
+		departamentoRepository.delete(depatamento);
+	}
+	@PutMapping("/departamento")
+	public Departamento atualizaDepartamento(@RequestBody Departamento departamento) {
 		return departamentoRepository.save(departamento);
 	}
 }
--- src/main/java/com/delphos/irpf/repository/DepartamentoRepository.java
+++ src/main/java/com/delphos/irpf/repository/DepartamentoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.delphos.irpf.model.Departamento;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Long>{
-
+	Departamento findById(long idDepatamento);
 }
