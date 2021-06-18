ee7ef8ff0a2f402f1a86dbf0c22caa2be8b75d89
Pablo Gonzalez <pablojava853@outlook.com>
2021-06-18 04:06:44 -0300

    Criação dos metodos salvar, deletar, listar todos,atualizar e listar unico funcionario

--- src/main/java/com/delphos/irpf/controller/FuncionarioController.java
+++ src/main/java/com/delphos/irpf/controller/FuncionarioController.java
@@ -1,6 +1,13 @@
 package com.delphos.irpf.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +22,24 @@ public class FuncionarioController {
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
 	
+	@PostMapping("/funcionario")
 	public Funcionario salvarFunconario(@RequestBody Funcionario funcionario) {
 		return funcionarioRepository.save(funcionario);
 	}
-
+	@GetMapping("/funcionarios")
+	public List<Funcionario>funcionarios(){
+		return funcionarioRepository.findAll();
+	}
+	@GetMapping("/funcionario/{id}")
+	public Funcionario listaUnicoFuncionario(@PathVariable(value="id") long id) {
+		return funcionarioRepository.findById(id);
+	}
+	@DeleteMapping("/funcionario")
+	public void deletaFuncionario(@RequestBody Funcionario funcionario) {
+		funcionarioRepository.delete(funcionario);
+	}
+	@PutMapping("/funcionario")
+	public Funcionario atualizarFuncionario(@RequestBody Funcionario funcionario) {
+		return funcionarioRepository.save(funcionario);
+	}
 }
--- src/main/java/com/delphos/irpf/repository/FuncionarioRepository.java
+++ src/main/java/com/delphos/irpf/repository/FuncionarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.delphos.irpf.model.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-
+	Funcionario findById(long id);
 }
